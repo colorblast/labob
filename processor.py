44adefe3309d50987151dc3734a5ff77313a8756
Brian Isakov <brian-ac@outlook.com>
2016-05-25 14:51:11 -0400

    Update processor.py

--- processor.py
+++ processor.py
@@ -20,7 +20,7 @@ starterText = userInput['starterText'].value
 bodyText = userInput['body'].value
 fxn = userInput['function'].value
 landscape = userInput['landscape'].value
-
+paperstyle = userInput['paperStyle'].value
 
 
 latexc = tempfile.NamedTemporaryFile() # DO NOT Erase this line. Temporary file for LaTeX storage.
@@ -35,6 +35,13 @@ def namebreak():
 	lname=namelist[-1] # This assumes that the last name is next and last.
 if starter="MLA":
 	namebreak()
+
+# Quotation mark Correcter:
+def qcorrecter(string):
+	for x in string:
+		if x=='"':
+			#To-Do~
+		
 
 # Document Formatting Changes:
 if starter="Standard (recommended)":
@@ -61,12 +68,22 @@ if landscape=True:
 else:
 	lsss="%"
 
+# Begin paragraphs with an empty line rathen than an intent:
+if parseSkip=True:
+	psss=''
+else:
+	psss="%"
+
+# Quotation marks corrections
+if qCorrect=True:
+	qcorrecter()
+
 lbasic='''
 \documentclass[11pt, oneside]{'''articleclass'''}   	% use "amsart" instead of "article" for AMSLaTeX format
 \usepackage{geometry}                		% See geometry.pdf to learn the layout options. There are lots.
-\geometry{letterpaper}                   		% ... or a4paper or a5paper or ... 
+\geometry{'''paperstyle'''}                   		% ... or a4paper or a5paper or ... 
 '''lsss'''\geometry{landscape}                		% Activate for rotated page geometry
-%\usepackage[parfill]{parskip}    		% Activate to begin paragraphs with an empty line rather than an indent
+'''psss'''\usepackage[parfill]{parskip}    		% Activate to begin paragraphs with an empty line rather than an indent
 \usepackage{graphicx}				% Use pdf, png, jpg, or epsÂ§ with pdflatex; use eps in DVI mode
 								% TeX will automatically convert eps --> pdf in pdflatex		
 \usepackage{amssymb}
