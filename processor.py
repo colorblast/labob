cba21a7455e5651d4542b5b28cd46371b6630b03
Brian Isakov <brian-ac@outlook.com>
2016-05-27 14:49:55 -0400

    Update processor.py

--- processor.py
+++ processor.py
@@ -48,20 +48,20 @@ def qcorrecter(string):
 	yandex0=False
 	yandex1=False
 	for x in string:
-		if x == '"' and yandex0=False:
+		if x == '"' and yandex0==False:
 			string.replace(x,"``")
 			yandex0=True
-		if x == '"' and yandex0=True:
+		if x == '"' and yandex0==True:
 			string.replace(x,"''")
 			yandex0=False
-		if x == '“':
+		if x == '“': #WARNING! CHARACTER NOT SUPPORTED!
 			string.replace(x,"``")
 		if x == '”":
 			string.replace(x,"''")
-		if x == "'" and yandex1=False:
+		if x == "'" and yandex1==False:
 			string.replace(x,"`")
 			yandex1=True
-		if x == "'" and yandex1=True:
+		if x == "'" and yandex1==True:
 			yandex1=False
 		if x == "‘":
 			string.replace(x,"`")
