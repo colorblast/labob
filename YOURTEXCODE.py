e9d68422bd7ab3cb832aeb2a22f13542505db8a4
JonW27 <18wongj@gmail.com>
2016-06-04 13:34:20 -0400

    fix link ref thats failing

--- YOURTEXCODE.py
+++ YOURTEXCODE.py
@@ -252,7 +252,7 @@ def texman():
         g = open('output.tex', 'w')
         g.write(lbasic)
         g.close()
-        subprocess.call(['shell scripts/convertToPDF.sh'])
+        subprocess.call(['convertToPDF.sh'])
         TELE = redirectPDF+morestuff
         print TELE
     else:
