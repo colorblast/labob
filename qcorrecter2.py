87566195f53381c583a53e53cd6c205a5097270f
Brian Isakov <brian-ac@outlook.com>
2016-06-05 21:41:07 -0400

    qcorrecter2

--- qcorrecter2.py
+++ qcorrecter2.py
@@ -5,4 +5,21 @@ def qcorrecter2(string):
         if string[x]=='"' and yandex0==False:
             string=string[:x]+"``"+string[x+1:]
             yandex0=True
+        if string[x]=='"' and yandex0==True:
+            string=string[:x]+"''"+string[x+1:]
+            yandex0=False
+        if string[x]=="'" and yandex1==False:
+            string=string[:x]+"`"+string[x+1:]
+            yandex1==True
+        if string[x]=="'" and yandex1==True:
+            yandex1==False
+    for x in string:
+        if x == '“':
+            string.replace(x,"``")
+        if x == '”':
+            string.replace(x,"''")
+        if x == "‘":
+            string.replace(x,"`")
+        if x == "’":
+            string.replace(x,"'")
     return(string)
